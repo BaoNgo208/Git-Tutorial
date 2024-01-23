7bc7f29cd2db5b5672b8567176cf63e98d887180
BaoNgo208 <128871855+BaoNgo208@users.noreply.github.com>
2024-01-23 20:17:50 +0700

    update feature

--- hello.java
+++ hello.java
@@ -5,4 +5,8 @@ public class hello {
     public static void speak() {
          System.out.println("gau gau");
     }
+
+    public static void featureBranch1(){
+         System.out.println("this code is belong to feature branch");
+    }
 }
