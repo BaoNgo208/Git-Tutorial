e2f4193fcc075a54ac8e52fbab4bc549caca4d2d
BaoNgo208 <128871855+BaoNgo208@users.noreply.github.com>
2024-01-23 19:52:07 +0700

    3rd commit:add speak function

--- hello.java
+++ hello.java
@@ -2,4 +2,7 @@ public class hello {
     public static void print(){
          System.out.println("gekk");
     }
+    public static void speak() {
+         System.out.println("gau gau");
+    }
 }
